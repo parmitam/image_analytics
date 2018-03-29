e4ad599141b4a94571dd4016e3f72857a55201e5
Tomasz Pawlowski <tptpawlowski@gmail.com>
2018-03-29 16:17:38 +0200

    implemented data fetching for spark

--- neuroscience/spark/main.py
+++ neuroscience/spark/main.py
@@ -1,5 +1,9 @@
+import boto3
 import configparser
+import datetime
+import nibabel as nib
 import os
+import os.path as op
 
 
 def rekey(x):
@@ -7,6 +11,28 @@ def rekey(x):
     file_id = str(x[0].split("/")[7].split(".")[0])
     return (subject_id, file_id), x[1]
 
+def download(subject_id, output_directory='.'):
+    boto3.setup_default_session(profile_name='hcp')
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('hcp-openaccess')
+
+    file_name = 'data.nii.gz'
+    output_path = '{0}/{1}_{2}'.format(output_directory, subject_id, file_name)
+    if op.exists(output_path):
+        print("Using previously downloaded {0}".format(output_path))
+    else:
+        source_path = 'HCP/{0}/T1w/Diffusion/{1}'.format(subject_id, file_name)
+        print("Downloading {0} to {1}".format(source_path, output_path))
+        bucket.download_file(source_path, output_path)
+        print("Done")
+
+def downloadImages(x):
+    (subject_id, _), s3path = x
+    download(subject_id, '/tmp')
+    file_prefix = '/tmp/{0}_'.format(subject_id)
+    img = nib.load(file_prefix + 'data.nii.gz')
+    data = img.get_data()
+    return [((subject_id, str(i)), data[..., i]) for i in range(data.shape[-1])]
 
 def filterbvs(x):
     if x[0][1] == 'bvals' or x[0][1] == 'bvecs':
@@ -114,7 +140,7 @@ def regroup(x):
 def fit_model(dt, gtab):
     import dipy.reconst.dti as dti
     subjid = dt[0][0]
-    ten_model = dti.TensorModel(gtab.value[str(subjid)],min_signal=1)
+    ten_model = dti.TensorModel(gtab.value[str(subjid)], min_signal=1)
     ten_fit = ten_model.fit(dt[1][0], mask=dt[1][1])
     return (dt[0],ten_fit)
 
@@ -124,12 +150,13 @@ import sys
 if len(sys.argv) == 1:
     print("Please provide subject ids as parameters")
     sys.exit(0)
+subject_ids = sys.argv[1:]
 
 from pyspark import SparkContext, SparkConf
 conf = SparkConf()\
     .setAppName('neuroscience')\
     .setMaster('spark://127.0.0.1:7077')\
-    .setMaster('spark://127.0.0.1:7077')
+    .set('spark.executor.memory', '12g')
 sc = SparkContext(conf=conf)
 
 config_parser = configparser.ConfigParser()
@@ -139,43 +166,51 @@ config_parser.sections()
 sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", config_parser.get('hcp', 'AWS_ACCESS_KEY_ID'))
 sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", config_parser.get('hcp', 'AWS_SECRET_ACCESS_KEY'))
 
-s3Paths = ['s3a://hcp-openaccess/HCP/{0}/T1w/Diffusion/{1}'.format(subject_id, file_name) for subject_id in sys.argv[1:] for file_name in ['bvals', 'bvecs', 'data.nii.gz']]
-for path in s3Paths:
-    print("file in path: {0}".format(path))
 
-imgRDDs = [sc.binaryFiles(s3path, minPartitions=290) for s3path in s3Paths]
-imgRDD = sc.union(imgRDDs).map(rekey).cache()
-imgRDD.count()
+path_format = 's3a://hcp-openaccess/HCP/{0}/T1w/Diffusion/{1}'
+
+s3BvsPaths = [path_format.format(subject_id, file_name) for file_name in ['bvals', 'bvecs'] for subject_id in subject_ids]
+bvsRDD = sc.union([sc.binaryFiles(s3path) for s3path in s3BvsPaths]).map(rekey).repartition(16).cache()
+
+imgRDD = sc.parallelize([((subject_id, 'data.nii.gz'), 's3a://hcp-openaccess/HCP/{0}/T1w/Diffusion/{1}'.format(subject_id, 'data.nii.gz')) for subject_id in subject_ids])
+print("{0} initial partitions {1}, objects: {2}".format(datetime.datetime.now(), imgRDD.getNumPartitions(), imgRDD.count()))
+imgRDD = imgRDD.flatMap(downloadImages)
+print("{0} partitions after flatmap {1}, objects: {1}".format(datetime.datetime.now(), imgRDD.getNumPartitions(), imgRDD.count()))
+imgRDD = imgRDD.repartition(288).cache()
+print("{0} downloaded images {1} to partitions: {2}".format(datetime.datetime.now(), imgRDD.count(), imgRDD.getNumPartitions()))
 
 #get bvals and bvec files
-gtabRDD = imgRDD.filter(filterbvs)
+gtabRDD = bvsRDD
 gtabRDD = gtabRDD.map(rekeygtab)
 gtabRDD = gtabRDD.map(lambda nameTuple: (nameTuple[0], [ nameTuple[1] ])).reduceByKey(lambda a, b: a + b).cache()
-print(gtabRDD.take(3))
 gtabRDD = gtabRDD.map(buildgtab)
 
 gtb = gtabRDD.first()
 maskids = np.where(gtb[1].b0s_mask)
 broadcastVar = sc.broadcast(maskids)
+print("{0} read bvs".format(datetime.datetime.now()))
 
 # broadcast gtab:
 bcastGtab = sc.broadcast(gtabRDD.collectAsMap())
 
-imgOnlyRDD = imgRDD.filter(filterforimage).mapValues(unpick).cache()
-maskrawRDD = imgOnlyRDD.filter(filterformask).cache()
+maskrawRDD = imgRDD.filter(filterformask).cache()
 maskRDD = maskrawRDD.groupBy(lambda x: (x[0][0]))
 maskRDD1 = maskRDD.map(meanval)
 maskRDD2 = maskRDD1.map(lambda x: buildmask(x,bcastGtab))
 bcastMask = sc.broadcast(maskRDD2.collectAsMap())
+print("{0} masked".format(datetime.datetime.now()))
 
-
-denoisedRDD = imgOnlyRDD.map(lambda x:denoise(x,bcastMask))
+denoisedRDD = imgRDD.map(lambda x:denoise(x,bcastMask))
 denoisedRDD = denoisedRDD.flatMap(lambda x: repart(x, bcastMask))
 
-
+print("{0} denoised".format(datetime.datetime.now()))
 # reduce by key
 denoisedRDD = denoisedRDD.groupBy(lambda x: (x[0][0],x[0][1]))
+print("{0} grouped".format(datetime.datetime.now()))
 tmRDD = denoisedRDD.map(regroup)
+print("{0} regrouped".format(datetime.datetime.now()))
 
 tmmRDD = tmRDD.map(lambda x: fit_model(x,bcastGtab )).cache()
 tmmRDD.count()
+
+print("{0} done".format(datetime.datetime.now()))
